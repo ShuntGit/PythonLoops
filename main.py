2a85ce8aecd2258965a1f63aa4e083ff868cdbea
Simon Hunt <Azaern+Git@gmail.com>
2021-11-03 16:42:30 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# for each number in the array, append even if it's even, append odd if it's odd
+def example1(numbers):
+    message= []
+    #code goes here
+
+    return message
+
+# Append i to message as long as i is less than 12.
+def example2(target):
+    message=[]
+    i=2
+    #code goes here
+    return message
+
+# append all the squares of numbers from 0 up to target to message (e.g. target =4 0,1,4,9,16)
+def example3(target):
+    message=[]
+    #code goes here
+
+    return message
+
+
+
+
